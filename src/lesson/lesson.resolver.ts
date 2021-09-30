37ee1deabf4198026e8c01aec8203783e2da2623
Yamlak Kassahun <yamlak.k@gmail.com>
2021-09-30 17:23:41 +0300

    just add the lessons mutation and query

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -10,13 +10,10 @@ export class LessonResolver {
     ) {}
 
     @Query( returns => LessonType)
-    lesson() {
-      return {
-          id: 'sadhgasdhgasdh',
-          name: 'Computer Fundamentals',
-          startDate: (new Date()).toISOString(),
-          endDate: (new Date()).toISOString(),
-      }
+    lesson(
+      @Args('id') id: string,
+    ) {
+      return this.lessonService.getLesson(id);
     }
 
     @Mutation( returns => LessonType )
